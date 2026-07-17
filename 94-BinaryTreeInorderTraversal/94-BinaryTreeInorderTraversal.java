df44163947bde2286da9a344eec7d070ae5c664a
vasanth2134567 <24244062@drngpit.ac.in>
2026-07-17 14:57:48 +0530

    Create 94-BinaryTreeInorderTraversal

---
+++ 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.java
@@ -0,0 +1,31 @@
+// Last updated: 7/17/2026, 2:57:48 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private List<Integer> res = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        traverse(root);
+        return res;
+    }
+    private void traverse(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        traverse(root.left);
+        res.add(root.val);
+        traverse(root.right);
+    }
+}
