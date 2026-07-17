e13d82c5ac97c861d83f714c07ec675c90464258
vasanth2134567 <24244062@drngpit.ac.in>
2026-07-17 14:58:45 +0530

    Create 12-IntegerToRoman

---
+++ 12-IntegerToRoman/12-IntegerToRoman.java
@@ -0,0 +1,24 @@
+// Last updated: 7/17/2026, 2:58:45 PM
+class Solution {
+    public String intToRoman(int num) {
+         int[] values = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
+
+        String[] symbols = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
+         StringBuilder result = new StringBuilder();
+          for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                result.append(symbols[i]);
+                num -= values[i];
+            }
+          }
+          return result.toString();
+    }
+}
