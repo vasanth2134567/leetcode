4a823f922b3d1f3a3ab915da5c579962fed7b914
H Vasanth <24244062@drngpit.ac.in>
2026-09-21 21:13:19 +0530

    Create 148-SortList

---
+++ 148-SortList/version/java/148-SortList_20260921_211318.java
@@ -0,0 +1,16 @@
+// Last updated: 9/21/2026, 9:13:18 PM
+1class Solution {
+2    public String reverseWords(String s) {
+3        String[] words = s.split("\\s+");
+4        StringBuilder res = new StringBuilder();
+5
+6        for (int i = words.length - 1; i >= 0; i--) {
+7            res.append(words[i]);
+8            if (i != 0) {
+9                res.append(" ");
+10            }
+11        }
+12
+13        return res.toString().trim();
+14    }
+15}
